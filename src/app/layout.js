7116c615324134205e94f793bf147a9530b58164
MohamadSamsudin <mohamadsamsudin47@gmail.com>
2024-12-23 19:21:10 +0700

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,36 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Aras SPK",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline ml-10">
+        Hello, Welcome sammaDev
+      </h1>
+    </>
+  );
+}
